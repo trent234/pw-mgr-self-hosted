1a9461c294cf530cf034d98ef6d9c52836031231
trent wilson <trent234@gmail.com>
2020-02-14 00:11:25 -0800

    made client tweaks on local machine to get connection going

--- client/client.py
+++ client/client.py
@@ -6,25 +6,32 @@ import ssl
 import time 
 
 hostname = 'tsz.us'
-port = 1030
+port = 11030
 message = 'Hello, server'
 
 context = ssl.create_default_context()
 
+# create normal socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+# wrap the socket and ret an SSL socket that is tied to the context (settings & certs)
+connstream = context.wrap_socket(sock, server_hostname=hostname)
+print(connstream.version())
+
 # attempt to connect to the server
 while True:
     try:
-        sock = socket.create_connection((hostname, port), 5)
+        connstream.connect((hostname, port))
+        break;
     except ConnectionRefusedError as error: 
         print(error, ' Trying again...')
         time.sleep(2)
 
-# wrap the socket and ret an SSL socket that is tied to the context (settings & certs)
-ssock = context.wrap_socket(sock, server_hostname=hostname)
-print(ssock.version())
-
 # create ssl connection and do the work
-ssock.connect((hostnane, port))
-ssock.sendall(message)
-data = ssock.recvall()
+# ssock.connect((hostname, port))
+connstream.sendall(message.encode())
+data = connstream.recv(1024)
 print('Received: ', data)
+
+connstream.shutdown(socket.SHUT_RDWR)
+connstream.close()
+
