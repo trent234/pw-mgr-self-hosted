7b39994a839d0105645988c5de70b43b413ffb50
Trent Wilson <trent234@gmail.com>
2020-02-14 08:10:08 +0000

    got connection working

--- server/server.py
+++ server/server.py
@@ -3,9 +3,22 @@
 import socket
 import ssl
 import time 
+from datetime import datetime
 
-hostname = 'tsz.us'
-port = 1030
+# this is the actual server side program that best thing to do is probably move to seperate file
+def deal_with_client(connstream):
+    data = connstream.recv(1024)
+    # empty data means the client is finished with us
+    while data:
+        with open('log', 'a') as log:
+            now = datetime.now()
+            print('[', now.strftime("%m/%d/%Y, %H:%M:%S"), '] ', fromaddr, ': ', data, file=log)
+        connstream.sendall(b'Message logged')
+        data = connstream.recv(1024)
+
+# hostname = 'tsz.us'
+hostname = '192.168.1.100'
+port = 11030
 
 # set context
 context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
@@ -13,15 +26,16 @@ context.load_cert_chain('/etc/letsencrypt/live/tsz.us/fullchain.pem', '/etc/lets
 
 # create a new socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
-sock.bind(('tsz.us', port))
-sock.listen(1)
+sock.bind((hostname, port))
+sock.listen(2)
 
 # now deal with incoming connections from clients
 while True:
     # get the new socket from client side
-    newsocket, fromaddr = bindsocket.accept()
+    newsocket, fromaddr = sock.accept()
     with open('log', 'a') as log:
-        print('[', time.localtime(), '] ', fromaddr, ': Connected')
+        now = datetime.now()
+        print('[', now.strftime("%m/%d/%Y, %H:%M:%S"), '] ', fromaddr, ': Connected', file=log)
     # create server-side SSL socket for the connection 
     connstream = context.wrap_socket(newsocket, server_side=True)
     try:
@@ -30,13 +44,6 @@ while True:
         connstream.shutdown(socket.SHUT_RDWR)
         connstream.close()
     with open('log', 'a') as log:
-        print('[', time.localtime(), '] ', fromaddr, ': Disconnected')
+        now = datetime.now()
+        print('[', now.strftime("%m/%d/%Y, %H:%M:%S"), '] ', fromaddr, ': Disconnected', file=log)
 
-def deal_with_client(connstream):
-    data = connstream.recv(1024)
-    # empty data means the client is finished with us
-    while data:
-        with open('log', 'a') as log:
-            print('[', time.localtime(), '] ', fromaddr, ': ', data)
-        connstream.sendall(b'Message logged')
-        data = connstream.recv(1024)
