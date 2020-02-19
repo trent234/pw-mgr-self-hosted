98617597b9929bb87ec6586a0fbefaabbde3f624
trent wilson <trent234@gmail.com>
2020-02-19 00:09:29 -0800

    made more user friendly / cmd line params for config related variables / cleaned up code

--- client/client.py
+++ client/client.py
@@ -5,33 +5,48 @@ import socket
 import ssl
 import time 
 
-hostname = 'tsz.us'
-port = 11030
-message = 'Hello, server'
+# the actual communication between client/server once the connection is setup
+def actual_program(connstream):
+  message = 'Hello, server'
+  connstream.sendall(message.encode())
+  data = connstream.recv(1024)
+  print('Received: ', data)
 
-context = ssl.create_default_context()
+def usage():
+  print('usage: ./client.py hostname port')    
+  exit(0)
 
-# create normal socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
-# wrap the socket and ret an SSL socket that is tied to the context (settings & certs)
-connstream = context.wrap_socket(sock, server_hostname=hostname)
-print(connstream.version())
+def main():
+  if len(sys.argv) < 2: 
+    usage()    
 
-# attempt to connect to the server
-while True:
+  hostname = sys.argv[1] # tsz.us
+  port = int(sys.argv[2]) # 11030
+  
+  context = ssl.create_default_context()
+  
+  # create normal socket
+  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+  # wrap the socket and ret an SSL socket that is tied to the context (settings & certs)
+  connstream = context.wrap_socket(sock, server_hostname=hostname)
+  print(connstream.version())
+  
+  # attempt to connect to the server
+  while True:
     try:
-        connstream.connect((hostname, port))
-        break;
+      connstream.connect((hostname, port))
+      break;
     except ConnectionRefusedError as error: 
-        print(error, ' Trying again...')
-        time.sleep(2)
+      print(error, ' Try again? (y/n)')
+      response = input()
+      if (response != 'y'):
+        print('Goodbye.')
+        sys.exit()
+  
+  actual_program(connstream)  
+  
+  connstream.shutdown(socket.SHUT_RDWR)
+  connstream.close()
 
-# create ssl connection and do the work
-# ssock.connect((hostname, port))
-connstream.sendall(message.encode())
-data = connstream.recv(1024)
-print('Received: ', data)
-
-connstream.shutdown(socket.SHUT_RDWR)
-connstream.close()
-
+if __name__ == '__main__':
+  main()
