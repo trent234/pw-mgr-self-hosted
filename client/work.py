149da0228d55766380a4b83f2509f4b1a817e4a5
trent wilson <trent234@gmail.com>
2020-02-24 21:23:22 -0800

    client can send personalized msg

--- client/work.py
+++ client/work.py
@@ -1,8 +1,8 @@
 
 # the actual communication between client/server once the connection is setup
 def actual_program(connstream):
-  message = 'Hello, server'
-  connstream.sendall(message.encode())
+  user_input  = input('-->')
+  connstream.sendall(user_input.encode())
   data = connstream.recv(1024)
   print('Received: ', data)
 
