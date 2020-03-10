0bcebe3d133834af7a43f7f4dedbf4c109937a0d
trent wilson <trent234@gmail.com>
2020-03-10 01:54:24 -0700

    finished delete. made protocol consistent. verbose comments highlighting protocol.

--- client/client.py
+++ client/client.py
@@ -6,6 +6,10 @@ import socket
 import ssl
 import argparse
 
+def conn_cleanup(tls_sock):
+  tls_sock.shutdown(socket.SHUT_RDWR)
+  tls_sock.close()
+
 def main():
   parser = argparse.ArgumentParser(description='Retrieve a password securely.')
   parser.add_argument('--hostname', required=True, \
@@ -30,33 +34,51 @@ def main():
   context = ssl.create_default_context()
   tls_sock = context.wrap_socket(tcp_sock, server_hostname=args.hostname)
   
-  # attempt to connect to the server
+  # *** client interaction #1: attempt to connect to the server. 
+  # *** server interaction #1: server will respond with spam check 
+  # response: first char 0 = fail. 1 = success. followed by logs
   try:
     tls_sock.connect((args.hostname, args.port))
     print('[INFO]: ' + tls_sock.version())
   except ConnectionRefusedError as error: 
     print('[ERROR]: ' + str(error))
+    conn_cleanup(tls_sock)
     exit(1)
   
-  # *** client query #1: attempt to log in ***
+  # *** client interaction #2: attempt to log in 
   tls_sock.sendall((args.username + ' ' + args.password).encode())
-  login_response = tls_sock.recv(1024).decode()
-  success = login_response[0]
-  login_response = login_response[1:]
-  print(login_response)
+  server_response = tls_sock.recv(1024).decode()
+  success = server_response[0]
+  server_response = server_response[1:]
+  # *** server interaction #3: 0 means we failed login either bc of spam or bad login info. server conn closed.
   if success == 0:
+    conn_cleanup(tls_sock)
     exit(1)
 
-  # attempt to complete user's request 
+  # client interaction #3: attempt to complete user's request 
   if args.action.upper() == 'CREATE' or args.action.upper() == 'UPDATE':
     if len(args.account) != 2:
       print('[FAIL]: Two args needed for --account if --action == create or update. [newacct newpw] two strings were not detected.')
       exit(1)
     tls_sock.sendall((args.action + ' ' + args.account[0] + ' ' + args.account[1]).encode())
-    print(tls_sock.recv(1024).decode())
+  elif args.action.upper() == 'READ' or args.action.upper() == 'DELETE':
+    tls_sock.sendall((args.action + ' ' + args.account[0]).encode())
+  else:
+    print('[FAIL]: No valid action given. CREATE/READ/UPDATE/DELETE are you options.') 
+    conn_cleanup(tls_sock)
+    exit(1)
+    
+  server_response = tls_sock.recv(1024).decode()
+  divider = server_response.find(' ',0,len(server_response))
+  success = server_response[0:divider]
+  server_response = server_response[divider + 1:]
+
+  # *** server interaction #3: start with either fail/succes code or pw depending on request followed by logs. 
+  # see server comment for more info.
+  print(server_response)
+  print(success)
   
-  tls_sock.shutdown(socket.SHUT_RDWR)
-  tls_sock.close()
+  conn_cleanup(tls_sock)
 
 if __name__ == '__main__':
   main()
